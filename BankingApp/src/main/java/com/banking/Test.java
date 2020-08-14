c678fa1920f34fa63bc1a6dc5701ebea69116ce9
Aviator Yonathaniel <aviatoryona67@gmail.com>
2020-08-14 20:55:35 +0300

    Initial Commit

--- BankingApp/src/main/java/com/banking/Test.java
+++ BankingApp/src/main/java/com/banking/Test.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Test {
+package com.banking;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+class Test extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
