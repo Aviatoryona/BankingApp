c19e48459b8539d8c9a4f3828230fe6c0a1fccae
Aviator Yonathaniel <aviatoryona67@gmail.com>
2020-08-15 10:04:41 +0300

    Initial Commit

--- BankingApp/src/main/java/com/banking/Test.java
+++ BankingApp/src/main/java/com/banking/Test.java
@@ -1,15 +1,17 @@
 package com.banking;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "Test",urlPatterns = {"/test"})
 class Test extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("<h1>Hello there</h1>");
     }
 
     @Override
