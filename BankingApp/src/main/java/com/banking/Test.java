1c8554a687af252175ca2d210289cafcec69842f
Aviator Yonathaniel <aviatoryona67@gmail.com>
2020-08-15 10:48:34 +0300

    Initial Commit

--- BankingApp/src/main/java/com/banking/Test.java
+++ BankingApp/src/main/java/com/banking/Test.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Test",urlPatterns = {"/test"})
+@WebServlet(name = "Test", urlPatterns = {"/test"})
 class Test extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -16,6 +16,7 @@ class Test extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String user = req.getParameter("username");
+        resp.getWriter().println("Hello " + user);
     }
 }
