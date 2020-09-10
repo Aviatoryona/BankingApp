802e692b93a81abd3950c8493e3f5bae92e8a0fa
Aviator Yonathaniel <aviatoryona67@gmail.com>
2020-09-10 14:34:49 +0300

    Adding Hibernate, JPA and EJB

--- BankingApp/src/main/java/com/banking/Admin.java
+++ BankingApp/src/main/java/com/banking/Admin.java
@@ -17,12 +17,19 @@
  */
 package com.banking;
 
+import com.banking.logic.AdminLogic;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  *
@@ -30,15 +37,39 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "Admin", urlPatterns = {"/admin"})
 public class Admin extends HttpServlet {
-
+    
+    @Inject
+    AdminLogic adminLogic;
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (!StringUtils.isEmpty(action)) {
+            switch (action) {
+                case "100":  //get all customers
+                    GetAllCustomers(request, response);
+                    break;
+            }
+        }
     }
-
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     }
+    
+    void printResult(HttpServletResponse response, Object object) throws IOException {
+        try {
+            response.getWriter().write(
+                    new ObjectMapper().writeValueAsString(object)
+            );
+        } catch (JsonProcessingException ex) {
+            Logger.getLogger(Admin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
+    private void GetAllCustomers(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        printResult(response, adminLogic.getRegisteredCustomers(-1));
+    }
 }
