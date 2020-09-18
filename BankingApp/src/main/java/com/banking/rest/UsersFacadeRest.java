a4c551e6d14f573c3a6dca90a608b240927cd8a0
Aviator Yonathaniel <aviatoryona67@gmail.com>
2020-09-18 17:43:43 +0300

    Added REST

---
+++ BankingApp/src/main/java/com/banking/rest/UsersFacadeRest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2020 Aviator
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.banking.rest;
+
+import com.banking.entities.Users;
+import com.banking.logic.UsersLogic;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Aviator
+ */
+@Stateless
+@Path(value = "/users")
+public class UsersFacadeRest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @EJB
+    UsersLogic usersLogic;
+
+    @GET
+    @Produces(value = {MediaType.APPLICATION_JSON})
+    @Path(value = "/all")
+    public List<Users> getUsers() {
+        return usersLogic.getUsers(-1);
+    }
+}
